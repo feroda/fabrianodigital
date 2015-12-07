9247f8332976b936333ca01c6a8e6753f5b2c233
Radeox <radeox@RadeoxPC.lan>
2015-12-07 23:22:21 +0100

    Add preview for facebook, jpg, gif content

--- digitalxmas/templatetags/media_tags.py
+++ digitalxmas/templatetags/media_tags.py
@@ -1,15 +1,29 @@
-
 from django import template
 
 register = template.Library()
 
-
 @register.simple_tag
 def player(media):
     # TODO: preview for other kind of content
+    #Youtube video
     if media.url.find('youtube.com') != -1:
         youtube_id = media.url[media.url.find("?v=")+3:]
         embed_url = "http://www.youtube.com/embed/{}?autoplay=0".format(youtube_id)
         return '<iframe width="420" height="315" src="{}"> </iframe>'.format(embed_url)
+
+    #Facebook Video
+    elif media.url.find('facebook.com') != -1 and media.url.find('videos/') != -1:
+        facebook_id = media.url[media.url.find("videos/")+7:]
+        embed_url = "https://www.facebook.com/video/embed?video_id={}".format(facebook_id)
+        return '<iframe width="420" height="315" src="{}"> </iframe>'.format(embed_url)
+
+    #Generic JPG
+    elif media.url.find('.jpg') != -1:
+        return '<img src="{}" alt="{}" width="420" height="auto" style="border: 2px solid rgb(255, 255, 255);">'.format(media.url, media.title)
+
+    #Generic GIF
+    elif media.url.find('.gif') != -1:
+        return '<img src="{}" alt="{}" width="420" height="auto" style="border: 2px solid rgb(255, 255, 255);">'.format(media.url, media.title)
+
     else:
         return '<h3>{media.title}</h3><p>{media.url}'.format(media=media)
