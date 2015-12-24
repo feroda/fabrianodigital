cb743900b788de8920438290c0f063bb19e10e44
Luca Ferroni <luca@befair.it>
2015-12-24 02:40:08 +0100

    Riportato dal server l'implementazione del redirect della home

--- fabrianodigital/urls.py
+++ fabrianodigital/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from base import views
+
 urlpatterns = patterns(
     '',
+    url(r'^$', views.home),
     url(r'^admin/', include(admin.site.urls)),
     url(r'digitalxmas/', include('digitalxmas.urls')),
     url(r'api/v1/', include('api_v1.urls')),
